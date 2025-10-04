91fe8303ca78f163b70ca5e2ec59d1a22c7c645d
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2025-10-04 19:19:49 +0300

    Game functions

--- Moskovska-Olha/script.js
+++ Moskovska-Olha/script.js
@@ -18,6 +18,38 @@ function createCell(hasMine = false, adjacentMines = 0, state = CellState.CLOSED
     };
 }
 
+const directions = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1],
+];
+
+function getAdjacentCells(board, row, col) {
+    const cells = [];
+    const rows = board.length;
+    const cols = board[0].length;
+    for (const [dr, dc] of directions) {
+        const nr = row + dr;
+        const nc = col + dc;
+
+        if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+            cells.push(board[nr][nc]);
+        }
+    }
+    return cells;
+}
+
+function countAdjacentMines(board, row, col) {
+    let count = 0;
+    const rows = board.length;
+    const cols = board[0].length;
+    const adjacentCells = getAdjacentCells(board, row, col);
+    adjacentCells.forEach(cell => {
+        if (cell.hasMine) count++;
+    });
+    return count;
+}
+
 function createBoard(rows, cols, minesCount) {
     const board = [];
 
@@ -40,30 +72,38 @@ function createBoard(rows, cols, minesCount) {
         }
     }
 
-    const directions = [
-        [-1, -1], [-1, 0], [-1, 1],
-        [0, -1], [0, 1],
-        [1, -1], [1, 0], [1, 1],
-    ];
-
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
             if (board[r][c].hasMine) continue;
 
-            let count = 0;
-            for (const [dr, dc] of directions) {
-                const nr = r + dr;
-                const nc = c + dc;
-
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
-                    if (board[nr][nc].hasMine) count++;
-                }
-            }
-            board[r][c].adjacentMines = count;
+            board[r][c].adjacentMines = countAdjacentMines(board, r, c);
         }
     }
 
     return board;
+}
+
+function openCell(game, row, col) {
+    if(game.board[row][col].state !== CellState.CLOSED) return;
+    game.board[row][col].state = CellState.OPENED;
+    if(game.board[row][col].hasMine)
+    {
+        game.state = GameState.LOST;
+        return;
+    }
+    if(game.board[row][col].adjacentMines === 0)
+    {
+        const rows = game.board.length;
+        const cols = game.board[0].length;
+        for (const [dr, dc] of directions) {
+            const nr = row + dr;
+            const nc = col + dc;
+
+            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+                openCell(game, nr, nc);
+            }
+        }
+    }
 }
 
 function createGame(rows, cols, minesCount) {
@@ -76,6 +116,33 @@ function createGame(rows, cols, minesCount) {
     };
 }
 
-const testGame = createGame(5, 5, 5);
+function toggleFlag(game, row, col) {
+    if(game.board[row][col].state !== CellState.CLOSED) return;
+    game.board[row][col].state = CellState.FLAGGED;
+}
 
+function startTimer() {
+    let timerHandler = null;
+    let timer = 0;
+    timerHandler = setInterval(() => {
+        console.log(timer);
+        timer++;
+    }, 1000);
+    return timerHandler;
+}
+
+function stopTimer(timerHandler) {
+    console.log("Timer stopped");
+    clearInterval(timerHandler);
+}
+
+const testGame = createGame(5, 5, 5);
+openCell(testGame, 0, 0);
 console.log(testGame);
+openCell(testGame, 1, 2);
+toggleFlag(testGame, 2, 2);
+console.log(testGame);
+const timerHandler = startTimer();
+setTimeout(() => {
+    stopTimer(timerHandler);
+}, 1000);
