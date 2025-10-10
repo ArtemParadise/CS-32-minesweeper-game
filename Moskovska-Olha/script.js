b48f4ba671ef735f7e5422a16580e6a6978dc6f6
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2025-10-10 08:57:39 +0300

    Naming changes

--- Moskovska-Olha/script.js
+++ Moskovska-Olha/script.js
@@ -31,15 +31,16 @@ function createBoard(rows, cols, minesCount) {
 
     let placedMines = 0;
     while (placedMines < minesCount) {
-        const r = Math.floor(Math.random() * rows);
-        const c = Math.floor(Math.random() * cols);
+        const randomRow = Math.floor(Math.random() * rows);
+        const randomCol = Math.floor(Math.random() * cols);
 
-        if (!board[r][c].hasMine) {
-            board[r][c].hasMine = true;
+        if (!board[randomRow][randomCol].hasMine) {
+            board[randomRow][randomCol].hasMine = true;
             placedMines++;
         }
     }
 
+<<<<<<< Updated upstream
     const directions = [
         [-1, -1], [-1, 0], [-1, 1],
         [0, -1], [0, 1],
@@ -60,12 +61,45 @@ function createBoard(rows, cols, minesCount) {
                 }
             }
             board[r][c].adjacentMines = count;
+=======
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (board[row][col].hasMine) continue;
+
+            board[row][col].adjacentMines = countAdjacentMines(board, row, col);
+>>>>>>> Stashed changes
         }
     }
 
     return board;
 }
 
+<<<<<<< Updated upstream
+=======
+function openCell(game, row, col) {
+    if(game.board[row][col].state !== CellState.CLOSED) return;
+    game.board[row][col].state = CellState.OPENED;
+    if(game.board[row][col].hasMine)
+    {
+        game.state = GameState.LOST;
+        return;
+    }
+    if(game.board[row][col].adjacentMines === 0)
+    {
+        const rows = game.board.length;
+        const cols = game.board[0].length;
+        for (const [dirRow, dirCol] of directions) {
+            const neighborRow = row + dirRow;
+            const neighborCol = col + dirCol;
+
+            if (neighborRow >= 0 && neighborRow < rows && neighborCol >= 0 && neighborCol < cols) {
+                openCell(game, neighborRow, neighborCol);
+            }
+        }
+    }
+}
+
+>>>>>>> Stashed changes
 function createGame(rows, cols, minesCount) {
     return {
         rows,
